95643c274c5447b04d979c4e93f82b03b3d231bb
brianfu4u <43586811+brianfu4u@users.noreply.github.com>
2026-07-07 17:43:33 +0900

    S1-1: add packages/api/src/objects/objects.module.ts

---
+++ packages/api/src/objects/objects.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { ObjectsController } from './objects.controller';
+import { LinksController } from './links.controller';
+import { ObjectsService } from './objects.service';
+import { ObjectsRepository } from './objects.repository';
+import { RealtimeService } from './realtime.service';
+
+@Module({
+  controllers: [ObjectsController, LinksController],
+  providers: [ObjectsService, ObjectsRepository, RealtimeService],
+})
+export class ObjectsModule {}
