111862402401c1f18c6b941f6793e8955cc9de47
brianfu4u <43586811+brianfu4u@users.noreply.github.com>
2026-07-07 20:29:24 +0900

    S3: update packages/api/src/app.module.ts

--- packages/api/src/app.module.ts
+++ packages/api/src/app.module.ts
@@ -2,19 +2,23 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { HealthController } from './health/health.controller';
 import { DatabaseModule } from './database/database.module';
+import { EventsModule } from './events/events.module';
 import { ObjectsModule } from './objects/objects.module';
 import { ReportsModule } from './reports/reports.module';
 import { UploadsModule } from './uploads/uploads.module';
 import { VerificationModule } from './verification/verification.module';
+import { RecommendationModule } from './recommendations/recommendation.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
+    EventsModule,
     DatabaseModule,
     ObjectsModule,
     ReportsModule,
     UploadsModule,
     VerificationModule,
+    RecommendationModule,
   ],
   controllers: [HealthController],
 })
