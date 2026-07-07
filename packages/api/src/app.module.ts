4d4d11ff3d3378e5a424fdd451862d31fc722a35
brianfu4u <43586811+brianfu4u@users.noreply.github.com>
2026-07-07 18:48:51 +0900

    S1-2: update packages/api/src/app.module.ts

--- packages/api/src/app.module.ts
+++ packages/api/src/app.module.ts
@@ -3,9 +3,10 @@ import { ConfigModule } from '@nestjs/config';
 import { HealthController } from './health/health.controller';
 import { DatabaseModule } from './database/database.module';
 import { ObjectsModule } from './objects/objects.module';
+import { ReportsModule } from './reports/reports.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), DatabaseModule, ObjectsModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), DatabaseModule, ObjectsModule, ReportsModule],
   controllers: [HealthController],
 })
 export class AppModule {}
