5ed5676082d4e1b8e4b2736365f34cb911a46620
brianfu4u <43586811+brianfu4u@users.noreply.github.com>
2026-07-07 19:23:43 +0900

    S1-3 spec: update packages/api/src/uploads/uploads.module.ts

--- packages/api/src/uploads/uploads.module.ts
+++ packages/api/src/uploads/uploads.module.ts
@@ -1,16 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ObjectsModule } from '../objects/objects.module';
 import { UploadsController } from './uploads.controller';
 import { UploadsService } from './uploads.service';
 import { UploadsRepository } from './uploads.repository';
-import { STORAGE_PROVIDER } from '../storage/storage.provider';
+import { STORAGE_PORT } from '../storage/storage.provider';
 import { LocalDiskStorageProvider } from '../storage/local-disk.provider';
 
 @Module({
+  imports: [ObjectsModule], // for RealtimeService
   controllers: [UploadsController],
   providers: [
     UploadsService,
     UploadsRepository,
-    { provide: STORAGE_PROVIDER, useClass: LocalDiskStorageProvider },
+    { provide: STORAGE_PORT, useClass: LocalDiskStorageProvider },
   ],
 })
 export class UploadsModule {}
